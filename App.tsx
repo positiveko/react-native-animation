59e06aec2c1200d7d464c8a2ad201283c629f1c6
positiveko <edie.spark1er@gmail.com>
2021-11-04 14:37:12 +0900

    feat: panResponder setOffset 추가

--- App.tsx
+++ App.tsx
@@ -3,10 +3,20 @@ import { Animated, PanResponder } from 'react-native';
 import styled from 'styled-components/native';
 
 export default function App() {
-  // https://github.com/facebook/react-native/blob/main/packages/rn-tester/js/examples/PanResponder/PanResponderExample.js 참고
+  // 참고 https://github.com/facebook/react-native/blob/main/packages/rn-tester/js/examples/PanResponder/PanResponderExample.js
   const panResponder = useRef(
     PanResponder.create({
       onStartShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        console.log('Touch Started');
+        // dx, dy가 초기화되지 않도록 이전 위치를 받아와서 offset 설정.
+        POSITION.setOffset({
+          // Value는 실제로 number가 아니기 때문에 공식문서에서 _value로 접근하길 권장.
+          // TODO ... type 정의가 되지 않아서 후에 확인 필요
+          x: POSITION.x._value,
+          y: POSITION.y._value,
+        });
+      },
       onPanResponderMove: (_, { dx, dy }) => {
         // ((e: GestureResponderEvent, gestureState: PanResponderGestureState)
         POSITION.setValue({
@@ -15,14 +25,9 @@ export default function App() {
         });
       },
       onPanResponderRelease: () => {
-        Animated.spring(POSITION, {
-          toValue: {
-            x: 0,
-            y: 0,
-          },
-          bounciness: 20,
-          useNativeDriver: false,
-        }).start();
+        console.log('Touch Finished');
+        // touch가 끝날 때마다 offset을 초기화
+        POSITION.flattenOffset();
       },
     })
   ).current;
