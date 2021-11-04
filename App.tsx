8a4f91434e8d4e2c768b970dac191841153cf57d
positiveko <edie.spark1er@gmail.com>
2021-11-04 15:03:05 +0900

    feat: extrapolate 일 때 카드 사라지도록 애니메이션 추가

--- App.tsx
+++ App.tsx
@@ -4,45 +4,67 @@ import styled from 'styled-components/native';
 import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
+  // Values
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.Value(0)).current;
+  const rotation = position.interpolate({
+    inputRange: [-250, 250],
+    outputRange: ['-15deg', '15deg'],
+  });
+
+  // Animations
+  const onPressOut = Animated.spring(scale, {
+    toValue: 1,
+    useNativeDriver: true,
+  });
+  const onPressIn = Animated.spring(scale, {
+    toValue: 0.95,
+    useNativeDriver: true,
+  });
+  const goCenter = Animated.spring(position, {
+    toValue: 0,
+    useNativeDriver: true,
+  });
+
+  // Pan Responders
   // 참고 https://github.com/facebook/react-native/blob/main/packages/rn-tester/js/examples/PanResponder/PanResponderExample.js
   const panResponder = useRef(
     PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (_, { dx }) => {
+        console.log(dx);
         position.setValue(dx);
       },
-      onPanResponderGrant: () => onPressIn(),
-      onPanResponderRelease: () => {
-        // 연속으로 실행되기 때문에 parallel 사용
-        Animated.parallel([
-          onPressOut,
-          // touch 끝나면 원점으로 돌아오기
+      onPanResponderGrant: () => onPressIn.start(),
+      onPanResponderRelease: (_, { dx }) => {
+        // 한계치 이상으로 움직일 때 카드 사라지도록
+        if (dx < -320) {
           Animated.spring(position, {
-            toValue: 0,
+            toValue: -500,
             useNativeDriver: true,
-          }),
-        ]).start();
+          }).start();
+        } else if (dx > 320) {
+          Animated.spring(position, {
+            toValue: 500,
+            useNativeDriver: true,
+          }).start();
+        } else {
+          Animated.parallel([onPressOut, goCenter]).start();
+        }
       },
     })
   ).current;
-
-  const scale = useRef(new Animated.Value(1)).current;
-  const position = useRef(new Animated.Value(0)).current;
-
-  const onPressIn = () =>
-    Animated.spring(scale, { toValue: 0.95, useNativeDriver: true }).start();
-
-  const onPressOut = Animated.spring(scale, {
-    toValue: 1,
-    useNativeDriver: true,
-  });
 
   return (
     <Container>
       <Card
         {...panResponder.panHandlers}
         style={{
-          transform: [{ scale }, { translateX: position }],
+          transform: [
+            { scale },
+            { translateX: position },
+            { rotateZ: rotation },
+          ],
         }}>
         <Ionicons name='pizza' color='#192a56' size={98} />
       </Card>
@@ -54,7 +76,7 @@ const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: #ffdcaf;
+  background-color: #788d84;
 `;
 
 const Card = styled(Animated.createAnimatedComponent(View))`
