6c4b9acd7a23a2dab5335cd114d1a6d1f3d851de
positiveko <edie.spark1er@gmail.com>
2021-11-04 12:53:35 +0900

    feat: animatedComponent 추가

--- App.tsx
+++ App.tsx
@@ -1,6 +1,42 @@
 import React from 'react';
+import { Animated, TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
 
 export default function App() {
-  return null;
+  const Y = new Animated.Value(0);
+  const moveUp = () => {
+    Animated.spring(Y, {
+      toValue: -200,
+      bounciness: 20,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  Y.addListener(() => console.log(Y));
+
+  return (
+    <Container>
+      <TouchableOpacity onPress={moveUp}>
+        <AnimatedBox
+          style={{
+            transform: [{ translateY: Y }],
+          }}
+        />
+      </TouchableOpacity>
+    </Container>
+  );
 }
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled.View`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+
+const AnimatedBox = Animated.createAnimatedComponent(Box);
